541f776cf65a766b7314f9135a9e3a57e7672150
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-27 21:26:36 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,58 @@
+<?php
+    function print_title(){
+        if(isset($_GET['id'])){
+            echo $_GET['id'];
+        }else {
+            echo "Welcome";
+        }
+    }
+
+    function print_description(){
+        if(isset($_GET['id'])){
+            echo file_get_contents("data/".$_GET['id']);
+        }else{
+            echo "Hello, PHP";
+        }
+    }
+
+    function print_list(){
+        $list = scandir('./data');
+        $i = 0;
+        while($i < count($list)){
+            if($list[$i] != '.' && $list[$i] != '..'){                   
+                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+            $i++;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        <?php
+            print_title();
+        ?>
+    </title>
 </head>
 <body>
     <a href="index.html">Home</a>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
         <?php
-            $list = scandir('data');
+            print_list();
         ?>
     </ol>
     <h2>
         <?php
-            if(isset($_GET['id'])){
-                echo $_GET['id'];
-            }else {
-                echo "Welcome";
-            }              
+            print_title();
         ?>
     </h2>
     <?php
-        if(isset($_GET['id'])){
-            echo file_get_contents("data/".$_GET['id']);
-        }else{
-            echo "Hello, PHP";
-        }
+        print_description();
     ?>
 </body>
 </html>
