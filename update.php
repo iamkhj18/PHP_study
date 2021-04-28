86e9352ab39416dcfbeb6ae80977bc08e4f527ea
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:54:38 +0900

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     require('lib/print.php');
     require('view/top.php');
@@ -22,29 +21,4 @@
     </form>
 <?php
     require('view/bottom.php');
-=======
-<?php
-    require('lib/print.php');
-    require('view/top.php');
-?>
-    <a href="create.php">create</a>
-    <?php if(isset($_GET['id'])) { ?>
-    <a href="update.php?id=<?=$_GET['id']?>">update</a>
-    <?php } ?>
-
-    <form action="update_process.php" method="post">
-        <input type="hidden" name="old_title" value="<?=$_GET['id']?>">
-        <p>
-            <input type="text" name="title" placeholder="title" value="<?php print_title(); ?>">
-        </p>
-        <p>
-            <textarea name="description" placeholder="description"><?php print_description(); ?></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-    </form>
-<?php
-    require('view/bottom.php');
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 ?>
