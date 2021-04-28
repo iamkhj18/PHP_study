43e3b83fa1b549494bccb75da7a2d93b1efcc09d
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:54:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     require_once('lib/print.php');
     require('view/top.php');
@@ -21,28 +20,4 @@
     ?>
 <?php
     require('view/bottom.php');
-=======
-<?php
-    require_once('lib/print.php');
-    require('view/top.php');
-?>
-    <a href="create.php">create</a>
-    <?php if(isset($_GET['id'])) { ?>
-        <a href="update.php?id=<?=$_GET['id']?>">update</a>
-        <form action="delete_process.php" method="post">
-            <input type="hidden" name="id" value="<?=$_GET['id']?>">
-            <input type="submit" value="delete">
-        </form>
-    <?php } ?>
-    <h2>
-        <?php
-            print_title();
-        ?>
-    </h2>
-    <?php
-        print_description();
-    ?>
-<?php
-    require('view/bottom.php');
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 ?>
