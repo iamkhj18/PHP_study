d2126f597ba202a75cd7649c984a56e32050b560
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:51:34 +0900

    Update create_process.php

--- create_process.php
+++ create_process.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php
     file_put_contents('data/'.$_POST['title'], $_POST['description']);
     header('Location: /index.php?id='.$_POST['title']);
-=======
-<?php
-    file_put_contents('data/'.$_POST['title'], $_POST['description']);
-    header('Location: /index.php?id='.$_POST['title']);
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 ?>
