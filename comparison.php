f03568a4888e39f0a57582701c9d405027bc1d20
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:50:37 +0900

    Update comparison.php

--- comparison.php
+++ comparison.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -26,33 +25,4 @@
     var_dump(1>=1);
     ?>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.html">Home</a>   
-    <h1>Comparison Operators &amp; Boolean data type</h1>
-    
-    <h2>1==1</h2>
-    <?php
-    var_dump(1==1);
-    ?>
-
-    <h2>1>1</h2>
-    <?php
-    var_dump(1>1);
-    ?>
-
-    <h2>1>1</h2>
-    <?php
-    var_dump(1>=1);
-    ?>
-</body>
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 </html>
