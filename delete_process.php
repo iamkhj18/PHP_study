099f8f421406d87a7b0b02df21599cacbf07013f
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:51:56 +0900

    Update delete_process.php

--- delete_process.php
+++ delete_process.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php
     unlink('data/'.basename($_POST['id']));
     header('Location: /index.php');
-=======
-<?php
-    unlink('data/'.basename($_POST['id']));
-    header('Location: /index.php');
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 ?>
