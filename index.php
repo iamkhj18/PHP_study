52788b093034240a91706beaf695fb3929723eaa
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:00:20 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,51 +1,7 @@
 <?php
-    function print_title(){
-        if(isset($_GET['id'])){
-            echo $_GET['id'];
-        }else {
-            echo "Welcome";
-        }
-    }
-
-    function print_description(){
-        if(isset($_GET['id'])){
-            echo file_get_contents("data/".$_GET['id']);
-        }else{
-            echo "Hello, PHP";
-        }
-    }
-
-    function print_list(){
-        $list = scandir('./data');
-        $i = 0;
-        while($i < count($list)){
-            if($list[$i] != '.' && $list[$i] != '..'){                   
-                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-            $i++;
-        }
-    }
+    require_once('lib/print.php');
+    require('view/top.php');
 ?>
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        <?php
-            print_title();
-        ?>
-    </title>
-</head>
-<body>
-    <a href="index.html">Home</a>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-        <?php
-            print_list();
-        ?>
-    </ol>
     <a href="create.php">create</a>
     <?php if(isset($_GET['id'])) { ?>
         <a href="update.php?id=<?=$_GET['id']?>">update</a>
@@ -62,5 +18,6 @@
     <?php
         print_description();
     ?>
-</body>
-</html>
+<?php
+    require('view/bottom.php');
+?>
