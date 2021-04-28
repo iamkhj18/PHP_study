f94aa8983d6a957cb4c1ed690e5a8ee03d00cfb5
iamkhj18 <iamkhj18@naver.com>
2021-04-28 20:37:03 +0900

    PHP 기본문법

---
+++ comparison.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.html">Home</a>   
+    <h1>Comparison Operators &amp; Boolean data type</h1>
+    
+    <h2>1==1</h2>
+    <?php
+    var_dump(1==1);
+    ?>
+
+    <h2>1>1</h2>
+    <?php
+    var_dump(1>1);
+    ?>
+
+    <h2>1>1</h2>
+    <?php
+    var_dump(1>=1);
+    ?>
+</body>
+</html>
---
+++ loop.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.html">Home</a>
+    <h1>while</h1>
+    <?php
+        echo '1<br>';
+        $i = 0;
+        while($i < 3){            
+            echo '2<br>';
+            $i++;
+        }
+        echo '3<br>';
+    ?>
+</body>
+</html>
