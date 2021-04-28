ffe0ffe6d56d91c8b70a59799e25156aeaad2d1d
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:52:41 +0900

    Update form.php

--- form.php
+++ form.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <?php
     file_put_contents('data/'.$_POST['title'], $_POST['description']);
-=======
-<?php
-    file_put_contents('data/'.$_POST['title'], $_POST['description']);
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 ?>
