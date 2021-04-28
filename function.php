9677d1e8a1fb4663a88e08e57a775df61dad0c5e
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:53:08 +0900

    Update function.php

--- function.php
+++ function.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -26,33 +25,4 @@
     echo nl2br($str);
     ?>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.html">Home</a>
-    <h1>function</h1>
-    <?php
-        $str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Ad nisi magnam dolorem iusto sed sequi reiciendis harum cupiditate deleniti doloribus, ratione facilis a delectus obcaecati tenetur facere velit expedita culpa?";
-        echo  $str;
-    ?>
-
-    <h2>strlen()</h2>
-    <?php
-    echo strlen($str);
-    ?>
-    
-    <h2>nl2br</h2>
-    <?php
-    echo nl2br($str);
-    ?>
-</body>
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 </html>
