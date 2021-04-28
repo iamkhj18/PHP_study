9a50f8c4bb190c031c3112232a7a4f333d81e03e
iamkhj18 <69004912+iamkhj18@users.noreply.github.com>
2021-04-28 20:54:23 +0900

    Update loop.php

--- loop.php
+++ loop.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -20,27 +19,4 @@
         echo '3<br>';
     ?>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.html">Home</a>
-    <h1>while</h1>
-    <?php
-        echo '1<br>';
-        $i = 0;
-        while($i < 3){            
-            echo '2<br>';
-            $i++;
-        }
-        echo '3<br>';
-    ?>
-</body>
->>>>>>> ef8f80642e0fc09f227c21cd9cf3a2a530ba2634
 </html>
